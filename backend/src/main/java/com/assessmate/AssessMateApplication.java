cf56bc423e9eee92164e10b3417cb7a0ea4929e1
Thakkar Mitkumar Bharatbhai <meetthakkar9094@gmail.com>
2026-07-14 18:18:15 +0530

    Initial commit

---
+++ backend/src/main/java/com/assessmate/AssessMateApplication.java
@@ -0,0 +1,13 @@
+package com.assessmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessMateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/assessmate/AssessMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
